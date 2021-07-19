1462b7cdffabce490103ee161051d131e37ae5e9
Aniket <soodaniket028@gmail.com>
2021-07-19 22:34:42 +0530

    trial commit

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const rfs = require('rotating-file-stream');
 
-// Aniket & Aditya's Project   
+
 const logDirectory = path.join(__dirname,'../production_logs');
 fs.existsSync(logDirectory)||fs.mkdirSync(logDirectory);
 const accessLogStream = rfs.createStream('access.log',{
